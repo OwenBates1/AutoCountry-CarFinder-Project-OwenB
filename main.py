c6051446e023866f035670939a89da65f44d97d5
OwenBates1 <owbates117@gmail.com>
2024-07-28 11:14:46 +0000

    Project 0-3: CarFinder v0.3

--- main.py
+++ main.py
@@ -9,37 +9,38 @@ def user_menu():
       "*******************************\n")
   
 #Display menu with options
-
-user_menu()
-userChoice = input("Please Enter the following number below from the following menu:\n"
-                    "\n"  "1. PRINT all Authorized Vehicles"
-                    "\n" "2. SEARCH for Authorized Vehicle"
-                    "\n" "3. Exit ")
-
-
 def full_menu():
-  print("*******************************\n"
-     "AutoCountry Vehicle Finder v0.1\n"
-      "*******************************\n")
-  input("Please Enter the following number below from the following menu:\n"
-    "\n"  "1. PRINT all Authorized Vehicles"
-    "\n" "2. SEARCH for Authorized Vehicle"
-    "\n" "3. Exit ")
+    user_menu()
+    userChoice = input("Please Enter the following number below from the following menu:\n"
+      "1. PRINT all Authorized Vehicles\n"
+      "2. SEARCH for Authorized Vehicle\n"
+      "3. ADD Authroized Vehicle\n"
+      "4. Exit\n"
+      "Choice: ")
+    
+    return userChoice
+  
 
+while True:
+  userChoice = full_menu()
 
-
-if userChoice == "1":
-  for vehicle in AllowedVehiclesList:
-    print(vehicle)
-elif userChoice == "2":
-  x = input("PLEASE ENTER THE FULL VEHICLE NAME: ")
-  if x in AllowedVehiclesList:
-      print(x + " is an authorized vehicle")
-  else: 
-      print( x + " is not an authorized vehicle, if you received this in error please check the spelling and try again")
-  full_menu()
-elif userChoice == "3":
-  print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
-else:
-  print("Invalid entry please try again")
-
+  if userChoice == "1":
+    for vehicle in AllowedVehiclesList:
+        print(vehicle)
+  elif userChoice == "2":
+    x = input("PLEASE ENTER THE FULL VEHICLE NAME: ")
+    if x in AllowedVehiclesList:
+        print(x + " is an authorized vehicle")
+    else:
+        print(x + " is not an authorized vehicle. Please check the spelling and try again.")
+  elif userChoice == "3" :
+    newVehicle = input("Please Enter the full Vehicle name you would like to add: ")
+    AllowedVehiclesList.append(newVehicle) 
+    print("You have added " + newVehicle + " as an authorized vehicle")
+    
+  
+  elif userChoice == "4":
+    print("Thank you for using the AutoCountry Vehicle Finder. Goodbye!")
+    break
+  else:
+    print("Invalid entry. Please try again.")
