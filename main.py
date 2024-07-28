f24cb8a076829b6f8672ffecd36025b2bb34fc4b
OwenBates1 <owbates117@gmail.com>
2024-07-28 21:56:14 +0000

    CarFinder v1.0 GO LIVE

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 #MENU
 def user_menu():
   print("*******************************\n"
@@ -8,26 +7,21 @@ def user_menu():
 #Display menu with options
 def full_menu():
     user_menu()
-    userChoice = input("Please Enter the following number below from the following menu:\n"5
+    userChoice = input("Please Enter the following number below from the following menu:\n"
       "1. PRINT all Authorized Vehicles\n"
       "2. SEARCH for Authorized Vehicle\n"
-      "3. ADD Authrorized Vehicle\n"
-      "4. DELETE Authrorized Vehicle\n"
+      "3. ADD Authorized Vehicle\n"
+      "4. DELETE Authorized Vehicle\n"
       "5. Exit\n"
       "Choice: ")
     
     return userChoice
   
-
-while True:
-  
-  userChoice = full_menu()
-
-  if userChoice == "1":
-    print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ")
-    db = open("Vehicle.txt", "r")
-    print(db.read())
-  elif userChoice == "2":
+def print_vehicles():
+      print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ")
+      db = open("Vehicle.txt", "r")
+      print(db.read())
+def search_vehicles():
     x = input("PLEASE ENTER THE FULL VEHICLE NAME: ")
     db = open("Vehicle.txt", "r")
     if x in db.read():
@@ -35,12 +29,12 @@ while True:
     else:
         print(x + " is not an authorized vehicle. Please check the spelling"
               " and try again.")
-  elif userChoice == "3" :
+def add_vehicles():
     newVehicle = input("Please Enter the full Vehicle name you would like to add: ")
     db = open("Vehicle.txt", "a")
     db.write(newVehicle + "\n")
     print("You have added " + newVehicle + " as an authorized vehicle")
-  elif userChoice == "4":
+def remove_vehicles():
     removeVehicle = input("Please Enter the full Vehicle name" 
                           " you would like to REMOVE: ")
     confirmed = input("Are you sure you want to remove " + removeVehicle + 
@@ -52,14 +46,19 @@ while True:
       for line in lines:
         if line.strip("\n") != removeVehicle:
           db.write(line)
-          
-    
-      
-       
-    if confirmed == "no" "No":
-          continue
+
+while True:
+  userChoice = full_menu()
+  if userChoice == "1":
+    print_vehicles()
+  elif userChoice == "2":
+    search_vehicles()
+  elif userChoice == "3":
+    add_vehicles()
+  elif userChoice == "4":
+    remove_vehicles()
   elif userChoice == "5":
-    print("Thank you for using the AutoCountry Vehicle Finder. Goodbye!")
+    print("Thank you for using AutoCountry Vehicle Finder, Goodbye!")
     break
   else:
-    print("Invalid entry. Please try again.")
+    print("Invalid choice, please try again")
